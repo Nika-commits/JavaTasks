fd90a57071019cdb1cb26ada51c306510fac350e
Your Name <itismepranish@gmail.com>
2026-06-29 14:09:03 +0545

    added basic operations for calculations

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public static int Add( int number1, int number2){
+        return number1 + number2;
+    }
+
+    public static int Substract(int number1, int number2){
+        return number1 - number2;
+    }
+
+    public static int Multiply(int number1, int number2){
+        return number1 * number2;
+    }
+
+    public static int Divide(int number1, int number2){
+        return number1/number2;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Enter the First number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Enter the First number: ");
+        int number2 = scanner.nextInt();
+
+        System.out.println("Enter the type of Calculation you want to perform ? (Add, Subtract, Multiply, Divide");
+
+        String typeOfCalc = scanner.next().toLowerCase();
+
+        switch (typeOfCalc){
+            case "add" -> System.out.println(Calculator.Add(number1, number2));
+            case "subtract" -> System.out.println(Calculator.Substract(number1 , number2));
+            case "multiply" -> System.out.println(Calculator.Multiply(number1, number2));
+            case "divide" -> System.out.println(Calculator.Divide(number1 , number2));
         }
+
     }
 }
+
