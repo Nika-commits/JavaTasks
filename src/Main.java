b96912e7db56670bbc541562b07f68a0cd497eba
Your Name <itismepranish@gmail.com>
2026-06-29 14:55:12 +0545

    added loop to the program

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,49 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        boolean toContinue = false;
 
-        try {
-        System.out.println("Enter the First number: ");
-        int number1 = scanner.nextInt();
+        do {
+            try {
+                System.out.println("Enter the First number: ");
+                int number1 = scanner.nextInt();
 
-        System.out.println("Enter the Second number: ");
-        int number2 = scanner.nextInt();
+                System.out.println("Enter the Second number: ");
+                int number2 = scanner.nextInt();
 
-        System.out.println("Enter the type of Calculation you want to perform ? (Add, Subtract, Multiply, Divide)");
+                System.out.println("Enter the type of Calculation you want to perform ? (Add, Subtract, Multiply, Divide)");
 
-            String typeOfCalc = scanner.next().toUpperCase();
-            Calculations calculations = Calculations.valueOf(typeOfCalc);
+                String typeOfCalc = scanner.next().toUpperCase();
+                Calculations calculations = Calculations.valueOf(typeOfCalc);
 
-            switch (calculations) {
-                case ADD ->
-                        System.out.println("Addition: " + Calculator.Add(number1, number2));
-                case SUBTRACT ->
-                        System.out.println("Subtraction: " + Calculator.Subtract(number1, number2));
-                case MULTIPLY ->
-                        System.out.println("Multiplication: " + Calculator.Multiply(number1, number2));
-                case DIVIDE ->
-                        System.out.println("Division: " + Calculator.Divide(number1, number2));
+                switch (calculations) {
+                    case ADD ->
+                            System.out.println("Addition: " + Calculator.Add(number1, number2));
+                    case SUBTRACT ->
+                            System.out.println("Subtraction: " + Calculator.Subtract(number1, number2));
+                    case MULTIPLY ->
+                            System.out.println("Multiplication: " + Calculator.Multiply(number1, number2));
+                    case DIVIDE ->
+                            System.out.println("Division: " + Calculator.Divide(number1, number2));
+                }
+             } catch (
+                            java.util.InputMismatchException ex) {
+                System.out.println("Please input numbers");
+                    } catch     (
+                    IllegalArgumentException ex) {
+                System.out.println("Invalid Calculation Type. ");
+            } catch (
+                    ArithmeticException ex) {
+                System.out.println("Cannot perform calculation");
             }
-        }
-        catch (java.util.InputMismatchException ex) {
-            System.out.println("Please input numbers");
-        }
-        catch(IllegalArgumentException ex){
-            System.out.println("Invalid Calculation Type. ");
-        }
-        catch (ArithmeticException ex){
-            System.out.println("Cannot perform calculation");
-        }
+
+            System.out.println("Do you want to calculate again ? (Y)\nPress other to exit");
+
+            String answer = scanner.next().toLowerCase();
+
+            toContinue = answer.equals("y");
+
+        } while (toContinue);
     }
 }
 
