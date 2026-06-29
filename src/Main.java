e3eeea51df19335d4fcf1ac2594e3b9c94571bd8
Your Name <itismepranish@gmail.com>
2026-06-29 14:24:17 +0545

    refined output with string interpolation

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,21 @@ public class Main {
         System.out.println("Enter the First number: ");
         int number1 = scanner.nextInt();
 
-        System.out.println("Enter the First number: ");
+        System.out.println("Enter the Second number: ");
         int number2 = scanner.nextInt();
 
-        System.out.println("Enter the type of Calculation you want to perform ? (Add, Subtract, Multiply, Divide");
+        System.out.println("Enter the type of Calculation you want to perform ? (Add, Subtract, Multiply, Divide)");
 
         String typeOfCalc = scanner.next().toUpperCase();
         Calculations calculations = Calculations.valueOf(typeOfCalc);
-        switch (calculations){
-            case ADD -> System.out.println(Calculator.Add(number1, number2));
-            case SUBTRACT -> System.out.println(Calculator.Substract(number1 , number2));
-            case MULTIPLY -> System.out.println(Calculator.Multiply(number1, number2));
-            case DIVIDE -> System.out.println(Calculator.Divide(number1 , number2));
-        }
 
+
+        switch (calculations){
+            case ADD -> System.out.println("Addition: " + Calculator.Add(number1, number2));
+            case SUBTRACT -> System.out.println("Subtraction: " + Calculator.Substract(number1 , number2));
+            case MULTIPLY -> System.out.println("Multiplication: " + Calculator.Multiply(number1, number2));
+            case DIVIDE -> System.out.println("Division: " + Calculator.Divide(number1 , number2));
+        }
     }
 }
 
