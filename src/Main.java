f10420b9789f3ce507e5c5beb8ed460eeea1e67a
Your Name <itismepranish@gmail.com>
2026-06-29 14:29:25 +0545

    added input to try block to handle input exceptions

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
+        try {
         System.out.println("Enter the First number: ");
         int number1 = scanner.nextInt();
 
@@ -12,20 +14,29 @@ public class Main {
 
         System.out.println("Enter the type of Calculation you want to perform ? (Add, Subtract, Multiply, Divide)");
 
-        try {
             String typeOfCalc = scanner.next().toUpperCase();
             Calculations calculations = Calculations.valueOf(typeOfCalc);
 
-            switch (calculations){
-                case ADD -> System.out.println("Addition: " + Calculator.Add(number1, number2));
-                case SUBTRACT -> System.out.println("Subtraction: " + Calculator.Substract(number1 , number2));
-                case MULTIPLY -> System.out.println("Multiplication: " + Calculator.Multiply(number1, number2));
-                case DIVIDE -> System.out.println("Division: " + Calculator.Divide(number1 , number2));
+            switch (calculations) {
+                case ADD ->
+                        System.out.println("Addition: " + Calculator.Add(number1, number2));
+                case SUBTRACT ->
+                        System.out.println("Subtraction: " + Calculator.Substract(number1, number2));
+                case MULTIPLY ->
+                        System.out.println("Multiplication: " + Calculator.Multiply(number1, number2));
+                case DIVIDE ->
+                        System.out.println("Division: " + Calculator.Divide(number1, number2));
             }
-        } catch(IllegalArgumentException ex){
+        }
+        catch (java.util.InputMismatchException ex) {
+            System.out.println("Please input numbers");
+        }
+        catch(IllegalArgumentException ex){
             System.out.println("Invalid Calculation Type. ");
         }
-
+        catch (ArithmeticException ex){
+            System.out.println("Cannot perform calculation");
+        }
     }
 }
 
